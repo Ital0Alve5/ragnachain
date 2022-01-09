4aec16fb81651b177c81c897b200db75d39cee19
italo <italo_alves_@outlook.com>
2022-01-09 16:26:47 -0300

    header fixado no layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,20 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpR fff">
+    <q-header reveal bordered class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
     <q-page-container>
       <router-view />
     </q-page-container>
+
   </q-layout>
 </template>
 
